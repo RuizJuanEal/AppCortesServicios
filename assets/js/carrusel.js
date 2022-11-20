fb6a97eeb6a0b3cbd1e010f51c421155fed0b262
Sebseb917 <juanrd971217@gmail.com>
2022-11-20 15:21:43 -0600

    :art: cambios

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,6 +1,29 @@
-var fotos = ['/images/fotopersona/persona1.png', '/images/fotopersona/persona2.png', '/images/fotopersona/persona3.png', '/images/fotopersona/persona4.png', '/images/fotopersona/persona5.png'],
-    cont= 0;
-// var names =     
+const clientes = [{
+    nombre: 'Dora',
+    localidad: 'Engativá',
+    testimonio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo natus libero mollitia sunt expedita consectetur iure, quas, magnam sint rem ipsum aperiam?',
+},{
+    nombre: 'Julio',
+    localidad: 'Teusaquillo',
+    testimonio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo natus libero mollitia sunt expedita consectetur iure, quas, magnam sint rem ipsum aperiam?',
+},{
+    nombre: 'Sandra',
+    localidad: 'Mártires',
+    testimonio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo natus libero mollitia sunt expedita consectetur iure, quas, magnam sint rem ipsum aperiam?',
+},{
+    nombre: 'Francisco',
+    localidad: 'Candelaria',
+    testimonio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo natus libero mollitia sunt expedita consectetur iure, quas, magnam sint rem ipsum aperiam?',
+},{
+    nombre: 'Cecilia',
+    localidad: 'Suba',
+    testimonio: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo natus libero mollitia sunt expedita consectetur iure, quas, magnam sint rem ipsum aperiam?',
+}]
+
+
+
+// var fotos = ['/images/fotopersona/persona1.png', '/images/fotopersona/persona2.png', '/images/fotopersona/persona3.png', '/images/fotopersona/persona4.png', '/images/fotopersona/persona5.png'],
+//     cont= 0; 
 
 function carrusel(contenedor){
     contenedor.addEventListener('click', e => {
@@ -11,18 +34,18 @@ function carrusel(contenedor){
 
         if(tgt == atras) {
             if(cont > 0){
-                img.src = fotos[cont - 1];
+                img.src = clientes[cont - 1];
                 cont--;
             } else {
-                img.src = fotos[fotos.length - 1];
-                cont = fotos.length - 1;
+                img.src = clientes[fotos.length - 1];
+                cont = clientes.length - 1;
             }
         } else if(tgt == adelante){
-            if(cont < fotos.length - 1){
-                img.src = fotos[cont + 1];
+            if(cont < clientes.length - 1){
+                img.src = clientes[cont + 1];
                 cont++;
             } else {
-                img.src = fotos[0];
+                img.src = clientes[0];
                 cont = 0;
             }
         }
---
+++ assets/js/dropdown.js
@@ -0,0 +1,13 @@
+const select = document.querySelector('#seleccion');
+const opciones = document.querySelector('#opciones');
+const seleccion = document.querySelector('#seleccion .contenido-seleccion');
+const hiddenInput = document.querySelector('#inputSelect');
+
+document.querySelectorAll('#opciones > #opcion').forEach((opcion) => {
+    console.log('Hola');
+});
+
+// select.addEventListener('click', () => {
+//     select.classList.toggle
+
+// })
